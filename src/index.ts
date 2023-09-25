814de8802b41f4d99ab42cb358ea49d4860e26c5
RobPruzan <rapruzan@buffalo.edu>
2023-09-24 22:25:46 -0400

    nuke api

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ import { app, server } from './api';
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, () =>
-  console.log(`API available on http://localhost:${port}`)
-);
+// app.listen(port, () =>
+//   console.log(`API available on http://localhost:${port}`)
+// );
 
 server.listen(port, () => {
   console.log(`Socket available on http://localhost:${port}`);
